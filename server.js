824a4e77261b42f810316e3f8d31dcbee6757245
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-01 20:58:51 -0400

    enabled routes variable and added express code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-// const routes = require('./controllers');
+const routes = require('./controllers');
 // const sequelize = require('./config/connection');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -20,6 +20,12 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/main.html');
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
 http.listen(PORT, () => {
-  console.log('Listening on port 3001');
+  console.log('HTTP Listening on port 3001');
 });
