a67858d5137547ad8d1b5c792c143eef4188bb4b
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-01 20:39:19 -0400

    created Profile model

---
+++ models/Profile.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Profile extends Model {}
+
+Profile.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'profile',
+  }
+);
+
+module.exports = Profile;
