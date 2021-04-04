0bdf764d5a2ea01fada2bc40b804c6bbf05d50f4
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-04 19:54:57 -0400

    added routes for profile and login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,38 @@
 const router = require('express').Router();
+const { Profile, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   res.render('chat');
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Profile }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
