4bb3cbe6b026d49672d04aff49d10500a9faa1dd
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-04 19:54:34 -0400

    added maxAge for cookie. will change time later.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: { maxAge: 60000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
