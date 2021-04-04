97369b1c4e4c0c70c149e806b6745b5761306284
Juan Sanchez <juan.sanchez@phila.gov>
2021-04-04 16:14:39 -0400

    added sequelize sessions. added helpers. console log port changed to template literal.

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const path = require('path');
 const express = require('express');
-const app = express();
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const exphbs = require('express-handlebars'); // requiring handlebars
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({}); //
+const hbs = exphbs.create({ helpers });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -27,8 +34,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
 sequelize.sync({ force: false }).then(() => {
   http.listen(PORT, () => {
-    console.log('HTTP Listening on port 3001');
+    console.log(`HTTP Listening on ${PORT}`);
   });
 });
