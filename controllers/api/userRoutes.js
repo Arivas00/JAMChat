be3938687647d8f7727009cf984f455f8e9f774a
Mohammad Rouf <mrouf82@gmail.com>
2021-04-05 21:21:26 -0400

    fixed login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,25 +16,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-
-
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { userName: req.body.userName } });
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
 
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect user name or password, please try again' });
+        .json({ message: 'Incorrect user name, please try again' });
       return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect user name or password, please try again' });
+      res.status(400).json({ message: 'Incorrect password, please try again' });
       return;
     }
 
--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,10 @@
 const loginFormHandler = async (event) => {
-  // Stop the browser from submitting the form so we can do so with JavaScript
   event.preventDefault();
 
-  // Gather the data from the form elements on the page
   const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
-    // Send the e-mail and password to the server
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
@@ -15,7 +12,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/chat');
     } else {
       alert('Failed to log in');
     }
